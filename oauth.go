1afeb447ada4958836eb4afb8298cb506fcc9362
Helder <helderfarias@gmail.com>
2015-03-18 02:30:36 -0300

    refactor

--- oauth.go
+++ oauth.go
@@ -2,6 +2,8 @@ package oauthprovider
 
 import (
 	"github.com/helderfarias/oauthprovider-go/server"
+	"github.com/helderfarias/oauthprovider-go/storage/memory"
+	"github.com/helderfarias/oauthprovider-go/token"
 )
 
 type OAuthServer struct {
@@ -12,7 +14,12 @@ func New() *OAuthServer {
 }
 
 func (o *OAuthServer) AuthorizationServer() *server.AuthorizationServer {
-	return server.NewAuthorizationServer()
+	newServer := server.NewAuthorizationServer()
+	newServer.AccessTokenStorage = &memory.MemoryAccessTokenStorage{}
+	newServer.RefreshTokenStorage = &memory.MemoryRefreshTokenStorage{}
+	newServer.ClientStorage = &memory.MemoryClientStorage{}
+	newServer.TokenType = &token.BearerTokenType{}
+	return newServer
 }
 
 func (o *OAuthServer) ResourceServer() *server.ResourceServer {
