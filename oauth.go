2d8b98b72707633789ba785952caced2f600c92c
helder <helderfarias@gmail.com>
2016-04-12 22:32:55 -0300

    scope storage

--- oauth.go
+++ oauth.go
@@ -1,30 +1,31 @@
 package oauthprovider
 
 import (
-    "github.com/helderfarias/oauthprovider-go/server"
-    "github.com/helderfarias/oauthprovider-go/storage/memory"
-    "github.com/helderfarias/oauthprovider-go/token"
+	"github.com/helderfarias/oauthprovider-go/server"
+	"github.com/helderfarias/oauthprovider-go/storage/memory"
+	"github.com/helderfarias/oauthprovider-go/token"
 )
 
 type OAuthServer struct {
 }
 
 func New() *OAuthServer {
-    return &OAuthServer{}
+	return &OAuthServer{}
 }
 
 func (o *OAuthServer) AuthorizationServer() *server.AuthorizationServer {
-    newServer := server.NewAuthorizationServer()
-    newServer.AccessTokenStorage = &memory.MemoryAccessTokenStorage{}
-    newServer.RefreshTokenStorage = &memory.MemoryRefreshTokenStorage{}
-    newServer.ClientStorage = &memory.MemoryClientStorage{}
-    newServer.TokenType = &token.BearerTokenType{}
-    return newServer
+	newServer := server.NewAuthorizationServer()
+	newServer.AccessTokenStorage = &memory.MemoryAccessTokenStorage{}
+	newServer.RefreshTokenStorage = &memory.MemoryRefreshTokenStorage{}
+	newServer.ClientStorage = &memory.MemoryClientStorage{}
+	newServer.ScopeStorage = &memory.MemoryScopeStorage{}
+	newServer.TokenType = &token.BearerTokenType{}
+	return newServer
 }
 
 func (o *OAuthServer) ResourceServer() *server.ResourceServer {
-    newServer := server.NewResourceServer()
-    newServer.AccessTokenStorage = &memory.MemoryAccessTokenStorage{}
-    newServer.TokenType = &token.BearerTokenType{}
-    return newServer
+	newServer := server.NewResourceServer()
+	newServer.AccessTokenStorage = &memory.MemoryAccessTokenStorage{}
+	newServer.TokenType = &token.BearerTokenType{}
+	return newServer
 }
